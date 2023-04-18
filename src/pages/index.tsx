7ddf660a5f1547ea4d089f3b901667d0a92b4786
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-04-19 01:41:21 +0700

    feat: finis home page hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,38 @@ import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 import styles from './index.module.css'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className={clsx('hero__title', styles.heroTextPrimary)}>
-          Hi, I'm Angga
-        </h1>
-        <p className={clsx('hero__subtitle', styles.heroText)}>
-          {siteConfig.tagline}
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx(
-              'button button--secondary button--lg',
-              styles.heroButtonPrimary
-            )}
-            to="/blog"
-          >
-            Read my blog
-          </Link>
+        <div className={clsx(styles.heroItems)}>
+          <div>
+            <h1 className={clsx('hero__title', styles.heroTextPrimary)}>
+              Hi, I'm Angga
+            </h1>
+            <p className={clsx('hero__subtitle', styles.heroText)}>
+              {siteConfig.tagline}
+            </p>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  'button button--secondary button--lg',
+                  styles.heroButtonPrimary
+                )}
+                to="/blog"
+              >
+                Read my blog
+              </Link>
+            </div>
+          </div>
+          <div>
+            <img src={useBaseUrl('/img/me.png')} />
+          </div>
         </div>
       </div>
     </header>
